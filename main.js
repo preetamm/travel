4b772b2e020b35980c4e50f58b02b9f63e9f08d3
preetamm <34884487+preetamm@users.noreply.github.com>
2020-04-30 16:27:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,14 @@ function success(pos) {
   console.log(`Latitude : ${crd.latitude}`);
   console.log(`Longitude: ${crd.longitude}`);
   console.log(`More or less ${crd.accuracy} meters.`);
+  var sceneEl = document.querySelector('a-scene');
   let model = document.createElement('a-entity');
   model.setAttribute('gps-entity-place', `latitude: ${crd.latitude}; longitude: ${crd.longitude};`);
   model.setAttribute('gltf-model', '../assets/scene.gltf');
   model.setAttribute('rotation', '0 180 0');
   model.setAttribute('animation-mixer', '');
   model.setAttribute('scale', '0.5 0.5 0.5');
+  sceneEl.appendChild(model);
 }
 
 function error(err) {
